9e5f8abce14b4289158556e71a973a6185a83830
Falodun Damilola <dpaulzs11@gmail.com>
2023-02-05 00:24:22 +0100

    added fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,8 @@ function Home() {
             <img src={desktopHeroImage} alt="finance photo" className="hidden md:block"/>
           </div>
           <article className="text-center text-white md:text-left md:flex-[60%]">
-            <h3 className="text-lg font-bold mb-2 md:text-[40px]">We teach you about finance</h3>
-            <p className="md:text-3xl">Financial information to achieve wealth creation, wealth sustainability, and wealth management</p>
+            <h3 className="text-lg mb-2 md:text-4xl benzin-bold">We teach you about finance</h3>
+            <p className="md:text-2xl benzin-regular">Financial information to achieve wealth creation, wealth sustainability, and wealth management</p>
           </article>
         </section>
         </div>
@@ -27,8 +27,8 @@ function Home() {
       {/* About section */}
       <section className="py-8 px-4 bg-lightblue about md:py-12">
         <article className="container mx-auto">
-          <h3 className="text-darkblue text-2xl">About Us</h3>
-          <h4 className="text-lg font-bold font-alternate">#On a Mission to drive Financial Independence.</h4>
+          <h3 className="text-darkblue text-2xl benzin-bold ">About Us</h3>
+          <h4 className="text-lg font-bold font-alternate my-2">#On a Mission to drive Financial Independence.</h4>
 
           <div className="flex flex-col gap-4 font-alternate md:text-xl">
             <p>03 Finance School was birthed as a result of the harm that lack of financial Information has caused many individuals and countries in Africa.</p>
@@ -42,9 +42,9 @@ function Home() {
       {/* Join Community Section */}
       <section className="py-8 px-4 bg-white">
         <article className="text-center bg-lightgreen p-4 md:w-1/2 md:mx-auto">
-          <p className="text-[#DBFFCD] text-lg md:text-3xl">Want to become a student?</p>
-          <h3 className="text-darkblue text-lg font-bold">JOIN OUR COMMMUNITIES</h3>
-          <p className="text-white font-alternate text-base md:text-xl md:my-8">Become a student and join other finance and blockchain enthusiast in our community. Share and gain ideas from different brains</p>
+          <p className="text-[#DBFFCD] text-lg benzin-medium md:text-2xl">Want to become a student?</p>
+          <h3 className="text-darkblue text-lg benzin-medium md:text-2xl">JOIN OUR COMMMUNITIES</h3>
+          <p className="text-white font-alternate text-base md:text-xl md:my-6">Become a student and join other finance and blockchain enthusiast in our community. Share and gain ideas from different brains</p>
           <div className="w-[100%] my-4 md:flex md:justify-around md:items-center">
             <button className="bg-white text-sharpblue flex gap-2 justify-center items-center px-4 mx-auto rounded-md">
               <p className="font-alternate font-[600] text-[16px]">Join our Telegram channel</p>
@@ -62,7 +62,7 @@ function Home() {
       {/* Updates Section */}
       <section className="bg-mobile-update-bg bg-no-repeat bg-cover text-white py-12 px-4 text-center font-alternate md:bg-desktop-update-bg">
         <article  className="container mx-auto flex flex-col gap-4 md:w-1/2 md:mx-auto md:py-6">
-          <h2 className="text-xl font-bold md:text-3xl">DONT WANT TO MISS OUT ON UPDATES?</h2>
+          <h2 className="text-xl benzin-bold md:text-3xl">DONT WANT TO MISS OUT ON UPDATES?</h2>
           <p className="md:text-xl md:my-6">Subscribe to our newsletter and get updated news on tokens, coins and other finance news</p>
           <div className="border border-sharpblue bg-sharpblue w-max mx-auto rounded-md overflow-hidden">
             <input type="email" name="email" id="email" 
