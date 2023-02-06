5d63eea2991a04417c7f3b5d96ba42d23d7fa23f
Falodun Damilola <dpaulzs11@gmail.com>
2023-02-06 15:20:56 +0100

    notfound fixed bug

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div className="bg-lightblue flex flex-col h-screen justify-center items-center font-alternate">
-        <div className='w-1/2 h-[60%]'>
+    <div className="bg-lightblue py-8 md:py-0 md:flex md:flex-col h-screen justify-center items-center font-alternate">
+        <div className='w-[80%] mx-auto md:w-1/2 md:h-[60%]'>
             <img src={notfound} alt="notfound" className='h-full mx-auto'/>
             <div className='text-center py-4'>
                 <h3 className='text-2xl benzin-medium'>Oopps, Page under production</h3>
